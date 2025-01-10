f5e45e32e2681d7daba588eadc0ebcd24b8d515d
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-01-11 02:21:04 +0600

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import CreateAdvertisement from "./pages/CreateAdvertisement";
 import OwnerAdvertisementsList from "./pages/OwnerAdvertisementsList";
 
 
 function App() {
   return (
-    <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<CreateAdvertisement />} />
-        <Route path="/my-houses/:owner_id" element={<OwnerAdvertisementsList />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <Outlet />
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import WebApp from '@twa-dev/sdk'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import './index.css'
 import App from './App.jsx'
+import OwnerAdvertisementsList from './pages/OwnerAdvertisementsList.jsx'
+import CreateAdvertisement from './pages/CreateAdvertisement.jsx'
 
 WebApp.ready();
-WebApp.expand();
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <CreateAdvertisement />,
+      },
+      {
+        path: "/my-houses",
+        element: <OwnerAdvertisementsList />,
+      }
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
