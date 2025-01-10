75700267b48d27968f2118fd96d7185473d47647
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-01-11 00:55:18 +0600

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateAdvertisement from "./pages/CreateAdvertisement";
 import OwnerAdvertisementsList from "./pages/OwnerAdvertisementsList";
 
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename="/nurbek19.github.io">
       <Routes>
         <Route path="/" element={<CreateAdvertisement />} />
         <Route path="/my-houses" element={<OwnerAdvertisementsList />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
