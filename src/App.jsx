d4dadaed1bbc82bb0d1bb50522a58272d3371333
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-01-10 22:09:14 +0600

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route index element={<CreateAdvertisement />} />
-        <Route path="my-houses" element={<OwnerAdvertisementsList />} />
+        <Route path="/" element={<CreateAdvertisement />} />
+        <Route path="/my-houses" element={<OwnerAdvertisementsList />} />
       </Routes>
     </HashRouter>
   )
