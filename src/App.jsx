c5e6802a9e19f1a4f25df3d3313d934ec72da13f
Nurbek Avtandilov <avtandilov06@gmail.com>
2025-01-11 01:44:09 +0600

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CreateAdvertisement />} />
-        <Route path="/my-houses/:owner_id" element={<OwnerAdvertisementsList />} />
+        <Route path="/my-houses" element={<OwnerAdvertisementsList />} />
       </Routes>
     </BrowserRouter>
   )
